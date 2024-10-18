69aaeae85537483a5cd3824e8e18db7a693c1ac5
Lycopenee <309334941@qq.com>
2024-10-18 02:03:20 -0500

    update the ig

--- manuly_task.py
+++ manuly_task.py
@@ -1,4 +1,4 @@
-from update_task import download_tif_file_by_index ,schedule_task_download_last_month, cancel_all_ee_tasks
+from update_task import download_tif_file_by_index ,schedule_task_download_last_month, cancel_all_ee_tasks,download_tif_file
 import time
 from datetime import datetime, timedelta
 #  set to the folder name in the drive
@@ -13,7 +13,7 @@ def download_by_index(index:int,source_type:str,start_date:str,end_date:str):
 
 
 def main():
-    schedule_task_download_last_month()
+    download_tif_file( 'nicfi', '2024-08-01', '2024-09-01')
 
 
 
